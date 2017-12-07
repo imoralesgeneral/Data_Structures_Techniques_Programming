c75b9389ba9b795096394cd496e127a5644bcf1b
imoralegeneral <imoralesgeneral@gmail.com>
2017-12-07 17:06:50 +0100

    Completing Test G

--- Structures_Techniques_Programming/test/com/structures/TestGraph/TestGraphD.java
+++ Structures_Techniques_Programming/test/com/structures/TestGraph/TestGraphD.java
@@ -4,61 +4,72 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.structures.Graph.GraphD;
+
 public class TestGraphD {
+	
+	GraphD g = new GraphD(5);
 
 	@Test
 	public void testNumVertex() {
-		fail("Not yet implemented");
+		assertEquals(5, g.numVertex());
 	}
 
-	@Test
+	/*@Test
 	public void testInsertVertex() {
-		fail("Not yet implemented");
-	}
+		g.insertVertex();
+		assertEquals(6, g.numVertex());
+	}*/
 
 	@Test
 	public void testNumEdges() {
-		fail("Not yet implemented");
+		g.insertEdge(1, 2);
+		g.insertEdge(2, 3);
+		assertEquals(2, g.numEdges());
 	}
 
 	@Test
 	public void testExistsEdge() {
-		fail("Not yet implemented");
+		g.insertEdge(1, 2);
+		g.insertEdge(2, 3);
+		assertEquals(true, g.existsEdge(2,3));
+		assertEquals(false, g.existsEdge(3,2));
 	}
 
 	@Test
 	public void testWeightEdge() {
-		fail("Not yet implemented");
+		g.insertEdge(1, 2);
+		g.insertEdgeP(4, 2, 3);
+		int aux1 = Integer.parseInt(Double.toString(g.weightEdge(1,2)));
+		int aux2 = Integer.parseInt(Double.toString(g.weightEdge(4,2)));
+		assertEquals(1, aux1, 0.1);
+		assertEquals(3, aux2, 0.1);
 	}
 
 	@Test
 	public void testInsertEdge() {
-		fail("Not yet implemented");
+		g.insertEdge(2, 3);
+		assertEquals(1, g.numEdges());
 	}
 
 	@Test
 	public void testInsertEdgeP() {
-		fail("Not yet implemented");
+		g.insertEdgeP(4, 2, 3);
+		assertEquals(1, g.numEdges());
 	}
 
 	@Test
 	public void testDeleteEdge() {
-		fail("Not yet implemented");
+		g.insertEdge(1, 2);
+		g.insertEdge(2, 3);
+		assertEquals(2, g.numEdges());
+		g.deleteEdge(2, 3);
+		assertEquals(1, g.numEdges());
 	}
-
-	@Test
-	public void testAdj() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	public void testGraphD() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+		assertNotNull(g);
 	}
 
 }
